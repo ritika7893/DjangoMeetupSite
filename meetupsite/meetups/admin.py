28f54f4e8f1054abeb01e41374321b38a1b1302e
ritika7893 <ritikakumari87344@gmail.com>
2025-04-15 22:11:32 +0530

    model addded

--- meetupsite/meetups/admin.py
+++ meetupsite/meetups/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Location, Participant, Meetup
 
+admin.site.register(Location)
+admin.site.register(Participant)
+admin.site.register(Meetup)
 # Register your models here.
